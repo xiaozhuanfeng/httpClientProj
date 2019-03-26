aa79c439fac49fe035d6a180e0c23024a2c6fad3
xiaozhuanfeng <627794853@qq.com>
2019-03-26 15:26:49 +0800

    HttpClient高并发测试用

---
+++ src/main/java/com/abp/util/HttpClientResult.java
@@ -0,0 +1,47 @@
+package main.java.com.abp.util;
+
+public class HttpClientResult {
+	/**
+	 * 响应状态码
+	 */
+	private int		code;
+
+	/**
+	 * 响应数据
+	 */
+	private String	content;
+	
+	public HttpClientResult() {
+	}
+	
+	public HttpClientResult(int code) {
+		this.code = code;
+	}
+	
+	public HttpClientResult(int code, String content) {
+		this.code = code;
+		this.content = content;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "HttpClientResult [code=" + code + ", content=" + content + "]";
+	}
+	
+}
---
+++ src/test/java/com/abp/HttpClientTest.java
@@ -0,0 +1,44 @@
+package test.java.com.abp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import main.java.com.abp.util.HttpClientResult;
+import main.java.com.abp.util.HttpClientUtils;
+
+import org.junit.Test;
+
+/**
+ * 用于高并发性能测试用
+ * @author xiaoz
+ *
+ */
+public class HttpClientTest {
+	
+	@Test
+	public void testGet(){
+		HttpClientResult result = null;
+		try {
+			result = HttpClientUtils.doGet("http://localhost:8080/hello");
+			System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testPost(){
+		HttpClientResult result = null;
+		try {
+			Map<String, String> params = new HashMap<String,String>();
+			params.put("username", "admin");
+			params.put("password", "123456");
+			//登录
+			result = HttpClientUtils.doPost("http://localhost:8080/login",params);
+			System.out.println(result);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
